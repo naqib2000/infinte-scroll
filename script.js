f19281b4c0c9e00a1f63370ee4d88266acb230cb
naqib2000 <muhammadnaqib2000@gmail.com>
2023-06-27 03:17:17 +0100

    Refactor display photos function

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const apiKey = config.MY_KEY;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 
 // create elements for links and photos and to DOM
 function displayPhotos() {
@@ -16,13 +21,17 @@ function displayPhotos() {
   photosArray.forEach((photo) => {
     // create <a> to link to unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // put <img> inside <a>, then put both inside image container element
     item.appendChild(img);
     imageContainer.appendChild(item);
